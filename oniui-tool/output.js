1dd38e7f62bac03a2177ea218437af8f28f5e1cb
qintx <rushi_wowen@163.com>
2015-08-10 23:12:54 +0800

    增加oniui打包

---
+++ oniui-tool/output.js
@@ -0,0 +1,9 @@
+define([
+	"dialog/avalon.dialog",
+	"button/avalon.button",
+	"doublelist/avalon.doublelist",
+	"draggable/avalon.draggable",
+	"tree/avalon.tree"
+	], function() {
+	// do nothing
+});
